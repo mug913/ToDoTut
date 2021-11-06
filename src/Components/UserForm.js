b13a1517a3b2796a53de5cd43bc62648147d1355
mug913 <brad.alexander913@gmail.com>
2021-11-06 15:25:11 -0700

    added email/pw validations to UserForm()

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -11,6 +11,7 @@ export function UserForm() {
     const handleSubmit = e => {
         e.preventDefault();
         var emailValid=false;
+        var passwordValid=false;
         if(email.length == 0){
             setEmailError("Email is required");
         }
@@ -21,9 +22,22 @@ export function UserForm() {
             setEmailError("")
             emailValid=true
         }
+
+        if(password.length < 6){
+            setPasswordError("Password should be at least six characters");
+        }
+        else if(password.length == 0){
+            setPasswordError("Password is required");
+        }
+        else{
+            setPasswordError("")
+            passwordValid=true
+        }
         
-        if(emailValid){
+        if(emailValid && passwordValid){
             alert('Email:' +email+' Passowrd:'+password);
+            setEmail("");
+            setPassword("");
         }
     }
     
@@ -32,16 +46,16 @@ export function UserForm() {
             <Form onSubmit = {handleSubmit}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+                    <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} value={email} />
                     <Form.Text className="text-muted">This info is Private</Form.Text>
                 </Form.Group>
                 {emailError.length > 0 && <Alert varient="danger">{emailError}</Alert>}
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} value={password}/>
                 </Form.Group>
-
+                {passwordError.length > 0 && <Alert varient="danger">{passwordError}</Alert>}
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
